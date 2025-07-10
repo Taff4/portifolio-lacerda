e69008d837c09108fc409918cd54942ba4a8bfe2
Rafael Lacerda Silva <167188437+Taff4@users.noreply.github.com>
2025-07-10 09:51:21 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { Navbar } from "../components/Navbar";
-// A importação do StarBackground foi removida daqui
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
@@ -10,7 +9,7 @@ import { Footer } from "../components/Footer";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-transparent text-foreground overflow-x-hidden">
-      {/* A linha do StarBackground foi removida daqui */}
+     
       
       <Navbar />
 
